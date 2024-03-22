b0748d07480ae73ee74053a5550cc5c588ba7b44
Japowneis <1ericyuji@gmail.com>
2024-03-22 20:32:48 -0300

    asd

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,5 +6,6 @@ class tests (models.Model):
     nivel_vestibular = models.CharField(max_length=30)
     nm_vestibular = models.CharField(max_length=20)
     ano_vestibular = models.IntegerField()
+    num_vestibular = models.IntegerField()
     url_vestibular = models.TextField()
     url_gabarito = models.TextField()
