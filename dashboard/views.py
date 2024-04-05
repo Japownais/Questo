178ecf0a28d7aaaf51f251321847c31049c7960c
Japowneis <1ericyuji@gmail.com>
2024-04-05 02:31:56 -0300

    EU VOU FICAR MALUCO

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,20 +3,22 @@ from django.contrib.auth.decorators import login_required
 from .forms import DeckForm, EventoForm
 from django.http import HttpResponse
 from .models import Evento
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
-from datetime import date
+from datetime import date, datetime
 import json
 from .models import Deck
-
-
+from django.utils.html import format_html
 
 # Create your views here.
 @login_required
 def profile(request):
     return render(request, 'profile.html')
 
+
 @login_required
+@csrf_exempt
 def calendar(request):    
     if request.method == 'POST':
         form = EventoForm(request.POST)
@@ -30,15 +32,29 @@ def calendar(request):
     else:
         form = EventoForm()
 
-    eventos = Evento.objects.filter(usuario=1)  # Busca todos os eventos do banco de dados filtrando o usuario
-    datas_eventos = [evento.data for evento in eventos]  # Lista de datas dos eventos
+    if request.method == 'POST':
+        data_selecionada = request.POST.get('dataAtiva')
+        data_selecionada = datetime.strptime(data_selecionada, '%Y-%m-%d').date()
 
-    print(" ")
-    print(datas_eventos)
+        eventos = Evento.objects.filter(usuario=request.user, data=data_selecionada)
 
-    context = {'form': form, 'datas_eventos': datas_eventos}
-    return render(request, 'calendar.html', context=context)
+        eventos_dict = [model_to_dict(evento) for evento in eventos]
 
+        lista_data_evento = [evento.data.strftime('%Y-%m-%d') for evento in eventos]
+
+        context = {'eventos': eventos_dict, 'lista_data_evento': lista_data_evento}
+        return JsonResponse(context)
+    else:
+        form = EventoForm()
+        eventos_marcados = Evento.objects.filter(usuario=request.user)
+        datas_eventos = [evento.data for evento in eventos_marcados]
+        lista_data_evento = [data.strftime('%Y-%m-%d') for data in datas_eventos]
+
+        eventos = Evento.objects.filter(usuario=request.user, data=date(2024, 4, 30))
+
+        context = {'form': form, 'datas_eventos': datas_eventos, 'lista_data_evento': lista_data_evento, 'eventos': eventos}
+        return render(request, 'calendar.html', context=context)
+        
 
 
 
@@ -48,11 +64,7 @@ def home(request):
     return render(request,'home.html')
 
 def flashcards(request):
-
     decks = Deck.objects.filter(usuario=request.user)
-
-
-
     if request.method == 'POST':
         deck_form = DeckForm(request.POST)  # Inicialize o formulário com os dados submetidos
         if deck_form.is_valid():  # Verifique se o formulário é válido
